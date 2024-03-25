6beb1dd460748927d72a178595f802b2ad0ad4b9
ahiranki <ahirankita213@gmail.com>
2024-03-25 13:23:17 +0530

    add

---
+++ 07-for-in-each/0701-print-below-output.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  var obj=['One','Two','Three'];
+  for(var x in obj)
+  {
+    print(x);
+  }
+}
---
+++ 07-for-in-each/0703-print-name.dart
@@ -0,0 +1,8 @@
+void main()
+{
+  var obj=['ronaldo','messi','Neymar','Hazard'];
+  for(var x in obj)
+  {
+    print(x);
+  }
+}
